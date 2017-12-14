31c60a39332a3101a787301d5ef35705044ad110
Stan Bondi <stan@fixate.it>
2017-12-14 17:35:49 +0200

    Allow empty predicate for required params

--- requiredParams.js
+++ requiredParams.js
@@ -3,28 +3,28 @@ const get = require('lodash/fp/get');
 
 const defaultOptions = {
   Error: errors.BadRequest,
+  emptyPredicate: data => f => typeof get(f, data) === 'undefined',
 };
 
 function getRequestData(hook) {
-	switch (hook.method) {
-		case 'get':
-			return hook.params;
-		case 'find':
-			return hook.params.query;
-		case 'create':
-		case 'update':
-		case 'patch':
-			return hook.data;
-		case 'remove':
-			return hook.params;
-		default:
-			return {};
-	}
+  switch (hook.method) {
+    case 'get':
+      return hook.params;
+    case 'find':
+      return hook.params.query;
+    case 'create':
+    case 'update':
+    case 'patch':
+      return hook.data;
+    case 'remove':
+      return hook.params;
+    default:
+      return {};
+  }
 }
 
 module.exports = function requiredParams(...fields) {
-  const opts = (typeof fields[fields.length - 1] === 'object') ?
-    fields.pop() : {};
+  const opts = typeof fields[fields.length - 1] === 'object' ? fields.pop() : {};
 
   const options = Object.assign({}, defaultOptions, opts);
   if (Array.isArray(fields[0])) {
@@ -37,11 +37,12 @@ module.exports = function requiredParams(...fields) {
     }
 
     const requestData = getRequestData(hook);
-    const missingFields = fields.filter(f => (typeof get(f, requestData) === 'undefined'))
+    const missingFields = fields.filter(emptyPredicate(requestData));
     if (missingFields.length > 0) {
-      const message = missingFields.length === 1 ?
-        `Required field ${fields[0]} is missing.` :
-        `Required fields ${fields.join(', ')} are missing.`
+      const message =
+        missingFields.length === 1
+          ? `Required field ${fields[0]} is missing.`
+          : `Required fields ${fields.join(', ')} are missing.`;
       throw new options.Error(message);
     }
 
